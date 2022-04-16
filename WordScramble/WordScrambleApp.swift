7e177b1967c2394ce600eb4a41c34eedc64bbc71
Shokri <shokre4@hotmail.com>
2022-04-17 00:59:05 +0400

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Shokri Alnajjar on 17/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
